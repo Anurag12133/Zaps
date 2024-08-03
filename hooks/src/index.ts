a2b68173dae3b2a8f998fd725cd83838ce93ae05
Anurag12133 <anurajput62616@gmail.com>
2024-08-03 17:50:28 +0530

    checking routes

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -6,9 +6,17 @@ const client = new PrismaClient();
 const app = express();
 app.post("/", async (req: any, res: any) => {
   const zapId = req.params.zapId;
-  await client.zapRun.create({
-    data: {
-      zapId: zapId,
-    },
+  const body = req.body;
+  await client.$transaction(async (tx) => {
+    const run = await client.zapRun.create({
+      data: {
+        zapId: zapId,
+      },
+    });
+    const zapOutbox = await client.zapOutbox.create({
+      data: {
+        zapRunId: run.id,
+      },
+    });
   });
 });
