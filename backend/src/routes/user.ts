b2bd3023598da8b55670bf932e9581f979abc0af
Anurag12133 <anurajput62616@gmail.com>
2024-08-05 22:04:22 +0530

    signin-signupp working added

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,18 +1,64 @@
 import { Router } from "express";
 import { Request, Response } from "express";
 import { authMiddleware } from "../authMidddleware";
+import { signupSchema } from "../types";
+import { prismaClient } from "../db";
 
 const router = Router();
 
-router.post("/sigup", (res: Request, req: Response) => {
-  console.log("Sign up");
+router.post("/sigup", async (req: Request, res: Response) => {
+  const body = req.body.email;
+  const parsedData = signupSchema.safeParse(body);
+
+  if (!parsedData) {
+    return res.json({
+      message: "Incorrect Credentials",
+    });
+  }
+  const ExsistingUser = await prismaClient.user.findFirst({
+    where: {
+      email: parsedData.data?.username,
+    },
+  });
+  if (ExsistingUser) {
+    return res.status(403).json({
+      message: "User Already exsist.",
+    });
+  }
+
+  await prismaClient.user.create({
+    data: {
+      email: parsedData.data?.username || "",
+      password: parsedData.data?.password || "",
+      name: parsedData.data?.name || "",
+    },
+  });
 });
 
-router.post("/sigin", (res: Request, req: Response) => {
-  console.log("Sign in");
+router.post("/sigin", async (req: Request, res: Response) => {
+  const body = req.body.email;
+  const parsedData = signupSchema.safeParse(body);
+
+  if (!parsedData) {
+    return res.json({
+      message: "Incorrect Credentials",
+    });
+  }
+  const user = await prismaClient.user.findFirst({
+    where: {
+      email: parsedData.data?.username,
+      password: parsedData.data?.password,
+    },
+  });
+
+  if (!user) {
+    return res.status(411).json({
+      message: "Wrong Credentials !",
+    });
+  }
 });
 
-router.get("/user", authMiddleware, (res: Request, req: Response) => {
+router.get("/user", authMiddleware, (req: Request, res: Response) => {
   console.log("Data");
 });
 
--- backend/src/types/index.ts
+++ backend/src/types/index.ts
@@ -1,17 +1,15 @@
 import { z } from "zod";
 
-export const signupInput = z.object({
+const signupSchema = z.object({
   username: z.string().email(),
   password: z.string().min(6),
   name: z.string().optional(),
 });
 
-export type SignupSchema = z.infer<typeof signupInput>;
-
-const signinInput = z.object({
+const signinSchema = z.object({
   username: z.string().email(),
   password: z.string().min(6),
   name: z.string().optional(),
 });
 
-export type SigninSchema = z.infer<typeof signinInput>;
+export { signinSchema, signupSchema };
